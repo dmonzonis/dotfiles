8197872e54fa81a4e02ff3c35c6ddfa8329ffb30
monzo94 <dmonzonis@protonmail.com>
2020-02-29 15:34:05 +0100

    Make i3lock screenshot more pixelated

--- scripts/i3pixlock.sh
+++ scripts/i3pixlock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png -scale 5% -scale 2000% /tmp/screen.png
 i3lock -i /tmp/screen.png
 rm /tmp/screen.png
